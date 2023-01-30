0a9f9701aab2139cdd4485e5e0a80d8fbcf56e14
bngh <masafumi@DESKTOP-C0S2H4V>
2023-01-31 01:24:05 +0900

    a

---
+++ test.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+	fetch('/test.txt') // (1) リクエスト送信
+	.then(response => response.text()) // (2) レスポンスデータを取得
+	.then(data => { // (3)レスポンスデータを処理
+
+		const file_area = document.getElementById('file_area');
+		file_area.innerHTML = data.replace(/\n/g, "<br>");
+
+	});
+});
