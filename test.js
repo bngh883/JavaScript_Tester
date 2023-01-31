c5e4f6cd69052ee9d03f859a3896b42a7156f869
bngh <masafumi@DESKTOP-C0S2H4V>
2023-02-01 01:30:09 +0900

    csvテスト

--- test.js
+++ test.js
@@ -4,8 +4,16 @@ window.addEventListener('DOMContentLoaded', function(){
 	.then(response => response.text()) // (2) レスポンスデータを取得
 	.then(data => { // (3)レスポンスデータを処理
 
-		const file_area = document.getElementById('file_area');
-		file_area.innerHTML = data.replace(/\n/g, "<br>");
+		const txt_area = document.getElementById('txt_area');
+		txt_area.innerHTML = data.replace(/\n/g, "<br>");
 
 	});
+
+	fetch('./FileBox/test.csv')
+	.then(response => response.text())
+	.then(data =>{
+		//let lines = data.split('\n');
+		const txt_area = document.getElementById('txt_area');
+		txt_area.innerHTML = data.replace(/\n/g, "<br>");
+	})
 });
