1ebe27a2d3ae7ebaabf4134dee6c40b7ba247919
bngh <masafumi@DESKTOP-C0S2H4V>
2023-05-12 18:30:22 +0900

    react

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 function MyButton() {
-	return (
-	  <button>
-		I'm a button
-	  </button>
-	);
-  }
-  
-  export default function MyApp() {
-	return (
-	  <div>
-		<h1>Welcome to my app</h1>
-		<MyButton />
-	  </div>
-	);
-  }
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
 
 window.addEventListener('DOMContentLoaded', function(){
 
